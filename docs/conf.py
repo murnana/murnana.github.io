e54a34d7b1e8063788771cfc60cd10adad4faadc
murnana <murnana@users.noreply.github.com>
2018-06-21 22:57:10 +0900

    move files

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 html_theme = 'theme'
-html_theme_path = ['.']
+html_theme_path = ['../']
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
