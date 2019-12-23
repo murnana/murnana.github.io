4f2b6d5c0503a30c3afa4905f1cfe7ca57ac9e10
murnana <murnana@users.noreply.github.com>
2019-12-23 23:39:21 +0900

    add styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from "react"
 import Menu from "./menu"
 
 export default ({ children }) => (
-  <div>
+  <div className="layout">
     <Menu />
     <main>{children}</main>
   </div>
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,50 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => (
+export default class Menu extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isOpen: false,
+    }
+  }
+
+  render() {
+    if (this.state.isOpen) {
+      return MenuList({
+        onClick: event => {
+          this.setState({ isOpen: !this.state.isOpen })
+        },
+      })
+    } else {
+      return MenuButton({
+        onClick: event => {
+          this.setState({ isOpen: !this.state.isOpen })
+        },
+      })
+    }
+  }
+}
+
+const MenuList = ({ onClick }) => (
   <nav className="menu">
-    <a href="/">Top</a>
+    <ul className="dialog">
+      <li>
+        <Link onClick={onClick} to="/">
+          Top
+        </Link>
+      </li>
+      <li>
+        <a href="#" onClick={onClick}>
+          Close
+        </a>
+      </li>
+    </ul>
   </nav>
+)
+
+const MenuButton = ({ onClick }) => (
+  <button className="menu" onClick={onClick}>
+    Menu
+  </button>
 )
